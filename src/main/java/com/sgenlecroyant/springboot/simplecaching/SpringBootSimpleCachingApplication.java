28cb91ecdd36858939cf64b2e03b12f5a8e590dd
sgenlecroyant <sgenlecroyant@gmail.com>
2022-03-29 15:04:22 +0200

    added initial dependencies

---
+++ src/main/java/com/sgenlecroyant/springboot/simplecaching/SpringBootSimpleCachingApplication.java
@@ -0,0 +1,19 @@
+package com.sgenlecroyant.springboot.simplecaching;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSimpleCachingApplication {
+	private static Logger logger = LoggerFactory.getLogger(SpringBootSimpleCachingApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSimpleCachingApplication.class, args);
+		
+		logger.info("Spring Boot Simple Caching Up and Running");
+	
+	}
+
+}
---
+++ src/test/java/com/sgenlecroyant/springboot/simplecaching/SpringBootSimpleCachingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sgenlecroyant.springboot.simplecaching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSimpleCachingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
